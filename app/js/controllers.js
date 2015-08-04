7f9b24ebbd19dbffb66c99fd3ac792260218f423
Aldo. D Perez <aldoshin@gmail.com>
2015-08-04 16:15:13 -0400

    merge

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,25 +5,25 @@
 var phonecatControllers = angular.module('phonecatControllers', []);
 
 phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-  }]);
+	function($scope, Phone) {
+		$scope.phones = Phone.query();
+		$scope.orderProp = 'age';
+	}]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
+	function($scope, $routeParams, Phone) {
+		$scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+			$scope.mainImageUrl = phone.images[0];
+		});
 
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    };
+		$scope.setImage = function(imageUrl) {
+			$scope.mainImageUrl = imageUrl;
+		};
 
-    $scope.hello = function (name) {
-    	alert('Hello ' + (name || 'world') + '!');
-    };
-  }]);
+		$scope.hello = function (name) {
+			alert('Hello ' + (name || 'world') + '!');
+		};
+	}]);
 
 
 phonecatControllers.controller('MainController', ['$scope','$route','$routeParams', '$location',
